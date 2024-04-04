c0a2e3f2eb86b17c1ec517b031678989005974b1
NgocTruong0701 <ngoctruongcv@gmail.com>
2024-04-05 01:19:37 +0700

    create erm, install and config database, Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => (
+        {
+          type: 'mysql',
+          host: configService.get('HOST'),
+          port: +configService.get('PORT'),
+          username: configService.get('USER_NAME'),
+          password: configService.get('PASSWORD'),
+          database: configService.get('DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
